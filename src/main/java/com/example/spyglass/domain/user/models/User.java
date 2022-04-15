27db2dd007c0b87e53a80f0fcc413f8f00c16247
michellemccool <mefiorella@gmail..com>
2022-04-15 09:56:07 -0400

    created new packages

---
+++ src/main/java/com/example/spyglass/domain/user/models/User.java
@@ -0,0 +1,22 @@
+package com.example.spyglass.domain.user.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @JsonIgnore
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String dateOfBirth;
+    private String password;
+
+
+
+}
