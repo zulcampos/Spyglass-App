7bc4415bb017dfb6917f7d3f217e224bda2d1785
manhakimcodes <amusa901@gmail.com>
2022-04-19 15:45:26 -0400

    pusher

--- src/main/java/com/example/spyglass/domain/user/controller/UserController.java
+++ src/main/java/com/example/spyglass/domain/user/controller/UserController.java
@@ -10,78 +10,77 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Id;
 import java.util.logging.Logger;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
-//@RestController
-//@RequestMapping("/user")
-//public class UserController {
-//    private static Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserController(UserController userController, UserService userService) {
-//        this.userService = userService;
-//
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private static Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserController userController, UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> createUserRequest(@RequestBody User user) {
+        User savedUser = userService.createUser(user);
+        ResponseEntity response = new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        return response;
+    }
+
+//        @GetMapping("")
+//    public ResponseEntity<User> getUser(){
+//        ResponseEntity<User> users = userService.;
+//        Iterable<User> users1 = new Iterable<>(users, HttpStatus.OK);
+//        return users1;
 //    }
-//
-//    @PostMapping("")
-//    public ResponseEntity<User> createUserRequest(@RequestBody User user) {
-//        User savedUser = userService.createUser(user);
-//        ResponseEntity response = new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-//        return response;
-//    }
-//
-////        @GetMapping("")
-////    public ResponseEntity<User> getUser(){
-////        ResponseEntity<User> users = userService.;
-////        Iterable<User> users1 = new Iterable<>(users, HttpStatus.OK);
-////        return users1;
-////    }
-//    @GetMapping("{/id}")
-//    public ResponseEntity<?> getProfileById(@PathVariable Integer id) {
-//        User user;
-//        try {
-//            user = userService.findById(1l);
-//            ResponseEntity<?> response = new ResponseEntity<>(user, HttpStatus.OK);
-//            return response;
-//        } catch (UserNotFoundException e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.NOT_FOUND)
-//                    .build();
-//        }
-//
-//            }
-//        @PutMapping("/{id}")
-//        public ResponseEntity<?> updateProfile(@PathVariable Integer Id,@RequestBody User user){
-//            try {
-//                User updatedUser = userService.updateUser(user);
-//                ResponseEntity response = new ResponseEntity<>(updatedUser, HttpStatus.OK);
-//                return response;
-//            } catch (UserNotFoundException e) {
-//                return ResponseEntity
-//                        .status(HttpStatus.NOT_FOUND)
-//                        .build();
-//            }
-//    }
-//        @DeleteMapping("/{id}")
-//        public ResponseEntity<?> deleteProfile(Integer id){
-//            try{
-//                userService.deleteUser(Long.valueOf(id));
-//                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-//            }catch (UserHasBeenDeleted e){
-//                return ResponseEntity
-//                        .status(HttpStatus.NOT_FOUND)
-//                        .build();
-//            }
-//
-//        }
-//}
-//
-//
-//
+    @GetMapping("{/id}")
+    public ResponseEntity<?> getProfileById(@PathVariable Integer id) {
+        User user;
+        try {
+            user = userService.findById(1l);
+            ResponseEntity<?> response = new ResponseEntity<>(user, HttpStatus.OK);
+            return response;
+        } catch (UserNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+
+            }
+        @PutMapping("/{id}")
+        public ResponseEntity<?> updateProfile(@PathVariable Integer Id,@RequestBody User user){
+            try {
+                User updatedUser = userService.updateUser(user);
+                ResponseEntity response = new ResponseEntity<>(updatedUser, HttpStatus.OK);
+                return response;
+            } catch (UserNotFoundException e) {
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .build();
+            }
+    }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<?> deleteProfile(Integer id){
+            try{
+                userService.deleteUser(Long.valueOf(id));
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }catch (UserHasBeenDeleted e){
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .build();
+            }
+
+        }
+}
+
+
+
 ////+UserController()
 ////        -Static: Logger
 ////        GetMapping
