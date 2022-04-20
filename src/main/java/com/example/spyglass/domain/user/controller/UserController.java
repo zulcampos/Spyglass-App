b3c68fa375994d770e4505593dc530418a6246b2
michellemccool <mefiorella@gmail..com>
2022-04-20 09:58:14 -0400

    updates

--- src/main/java/com/example/spyglass/domain/user/controller/UserController.java
+++ src/main/java/com/example/spyglass/domain/user/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-
 //@RestController
 //@RequestMapping("/user")
 public class UserController {
